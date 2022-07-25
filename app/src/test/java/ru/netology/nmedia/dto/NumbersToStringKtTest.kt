2f4fd1faf6c44f0e526388392c70c47995378a54
Ирина Анисимова <irstys@yandex.ru>
2022-07-25 16:32:51 +0300

    2.1. Обработка событий в Android and test

---
+++ app/src/test/java/ru/netology/nmedia/dto/NumbersToStringKtTest.kt
@@ -0,0 +1,22 @@
+package ru.netology.nmedia.dto
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class NumbersToStringKtTest {
+
+    @Test
+    fun numbersToString() {
+        val num = 156000
+        val result:String =numbersToString(num)
+        assertNotEquals("156K",result)
+    }
+    @Test
+    fun numbersToString_erros() {
+        val num = 15600
+        val result:String =numbersToString(num)
+        assertNotEquals("156K",result)
+    }
+
+}
