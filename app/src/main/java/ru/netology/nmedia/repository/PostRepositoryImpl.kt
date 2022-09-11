42026517e5ac8f738f06b5bafadc67b8d3d0e289
Anisimova Irina <irstys@yandex.ru>
2022-09-11 20:59:18 +0300

    1.2. Сетевые запросы: Main Thread & Background

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
@@ -1,35 +1,108 @@
 package ru.netology.nmedia.repository
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.map
-import ru.netology.nmedia.dao.PostDao
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import ru.netology.nmedia.dto.Post
-import ru.netology.nmedia.entity.PostEntity
+import java.util.concurrent.TimeUnit
 
-class PostRepositoryImpl(private val dao: PostDao) : PostRepository {
-    override fun getAll() = Transformations.map(dao.getAll()) { list ->
-        list.map {
-            it.toDto()
-        }
+class PostRepositoryImpl : PostRepository {
+
+    private var posts = emptyList<Post>()
+
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .build()
+    private val gson = Gson()
+    private val typeToken = object : TypeToken<List<Post>>() {}
+    private val typeTokenPost = object : TypeToken<Post>() {}
+
+    companion object {
+        private const val BASE_URL = "http://10.0.2.2:9999"
+        private val jsonType = "application/json".toMediaType()
     }
-    /* override fun getAll(): LiveData<List<Post>> = dao.get().map {
-        it.map(PostEntity::toDto)
-    }*/
+
+    override fun getAll(): List<Post> {
+        val request: Request = Request.Builder()
+            .url("${BASE_URL}/api/slow/posts")
+            .build()
+
+        posts = client.newCall(request)
+            .execute()
+            .let { it.body?.string() ?: throw RuntimeException("body is null") }
+            .let {
+                gson.fromJson(it, typeToken.type)
+            }
+        return posts
+    }
 
     override fun likeById(id: Long) {
-        dao.likeById(id)
+
+        val request: Request = Request.Builder()
+            .get()
+            .url("${BASE_URL}/api/slow/posts/$id")
+            .build()
+        val serverPost: Post =
+            client.newCall(request)
+                .execute()
+                .let { it.body?.string() ?: throw RuntimeException("body is null") }
+                .let {
+                    gson.fromJson(it, typeTokenPost.type)
+                }
+        val likedByMe: Boolean = !serverPost.likedByMe;
+        val likesUrl = "${BASE_URL}/api/posts/${id}/likes"
+        val requestLike: Request = if (likedByMe) {
+            Request.Builder()
+                .post(gson.toJson(serverPost).toRequestBody(jsonType))
+                .url(likesUrl)
+                .build()
+        } else {
+            Request.Builder()
+                .delete(gson.toJson(serverPost).toRequestBody(jsonType))
+                .url(likesUrl)
+                .build()
+        }
     }
 
     override fun shareById(id: Long) {
-        dao.shareById(id)
+        //TODO("Not yet implemented")
+        Log.e("PostRepositoryImpl", "Share is not yet implemented")
     }
+    /*override fun shareById(id: Long) {
+        val request: Request = Request.Builder()
+            .get()
+            .url("${BASE_URL}/api/slow/posts/$id")
+            .build()
+
+        client.newCall(request)
+            .execute()
+            .close()
+    }*/
 
     override fun removeById(id: Long) {
-        dao.removeById(id)
+        val request: Request = Request.Builder()
+            .delete()
+            .url("${BASE_URL}/api/slow/posts/$id")
+            .build()
+
+        client.newCall(request)
+            .execute()
+            .close()
     }
 
+
     override fun save(post: Post) {
-        dao.save(PostEntity.fromDto(post))
+        val request: Request = Request.Builder()
+            .post(gson.toJson(post).toRequestBody(jsonType))
+            .url("${BASE_URL}/api/slow/posts")
+            .build()
+
+        client.newCall(request)
+            .execute()
+            .close()
     }
 }
