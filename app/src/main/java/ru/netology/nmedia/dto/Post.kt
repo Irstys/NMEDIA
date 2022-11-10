4096c3b8f77f979c3e35018595fbf868b40b4706
Anisimova Irina <irstys@yandex.ru>
2022-11-10 23:26:42 +0300

    4.1. Загрузка и отображение изображений

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,6 +1,8 @@
 package ru.netology.nmedia.dto
-import Attachment
+
 import ru.netology.nmedia.enumeration.AttachmentType
+
+
 data class Post(
     val id: Long,
     val author: String,
@@ -12,8 +14,22 @@ data class Post(
     val attachment: Attachment? = null,
     val viewed: Boolean = false,
     val repost: Int = 0,
-    val views: Int=0,
+    val views: Int = 0,
     val video: String? = null,
 
-)
+    )
+
+data class Attachment(
+    val url: String,
+    //val description: String? = null,
+    val type: AttachmentType,
+) {
+    fun toDto() = Attachment(url, type)
+
+    companion object {
+        fun fromDto(dto: Attachment?) = dto?.let {
+            Attachment(it.url, it.type)
+        }
+    }
+}
 
